9346e49d77a45178858baa10b9fa57f0640ab04b
r0lo1@mail.ru <r0lo1@mail.ru>
2020-10-27 01:08:25 +0300

    Aded Page object

---
+++ pages/base_page.py
@@ -0,0 +1,9 @@
+#!/bin/env python3
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self): 
+        # ваша реализация
+        self.browser.get(self.url)
