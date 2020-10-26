5d0a228a6a131d3c61ce11a3da662ead983ab859
r0lo1@mail.ru <r0lo1@mail.ru>
2020-10-26 23:09:28 +0300

    first file upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # autotest-selenium-example1
+pytest -v --tb=line --language=en --browser_name=chrome test_main_page.py
---
+++ test_main_page.py
@@ -0,0 +1,6 @@
+#!/bin/env python3
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
